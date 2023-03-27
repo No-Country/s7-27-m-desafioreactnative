6b9ddce2429dc9d42ae7204e019729cf9cc1a084
Leandro Pereyra <me.leandrop@gmail.com>
2023-03-27 12:19:41 -0300

    homenavigation

---
+++ client/src/navigation/homeNavigation/HomeNavigation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../../screens/homeNavigation/Home";
+
+
+const HomeNavigation = ({ navigation }) => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+
+    </Stack.Navigator>
+  );
+};
+export default HomeNavigation;
