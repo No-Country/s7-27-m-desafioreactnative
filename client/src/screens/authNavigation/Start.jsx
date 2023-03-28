66b648a28eac03669883f4d231c704a86e11f94d
Leandro Pereyra <me.leandrop@gmail.com>
2023-03-28 12:26:41 -0300

    start screen + primarybutton

--- client/src/screens/authNavigation/Start.jsx
+++ client/src/screens/authNavigation/Start.jsx
@@ -1,16 +1,53 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const Start = () => {
+  const navigation = useNavigation();
   return (
-    <View style={{marginVertical:100, marginHorizontal:100}}>
-      <Text>START</Text>
-      <Text>START</Text>
-      <Text>START</Text>
-      <Text>START</Text>
-      <Text>START</Text>
-      <Text>START</Text>
+    <View style={styles.container}>
+      <View>
+        <View style={styles.whitebox}>
+          <Text style={{ color: "lightgray" }}>
+            Acá iría una imagen o logo me imagino
+          </Text>
+        </View>
+      </View>
+      <View>
+        <PrimaryButton
+          text="INGRESAR"
+          handler={() => {
+            navigation.navigate("LogIn");
+          }}
+        />
+        <PrimaryButton
+          text="REGISTRARSE"
+          handler={() => {
+            navigation.navigate("Register");
+          }}
+        />
+      </View>
     </View>
   );
 };
 
 export default Start;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#D9D9D9",
+    height: "100%",
+  },
+  whitebox: {
+    backgroundColor: "white",
+    height: 260,
+    width: 260,
+    marginBottom: 50,
+    marginTop: -40,
+  },
+});
