2c08d5cf5d864b8ed37c7f9408bfc7e91ffb4456
Lalomax <lalomax365@gmail.com>
2023-03-28 18:58:22 -0400

    register login markup started

--- client/src/screens/authNavigation/LogIn.jsx
+++ client/src/screens/authNavigation/LogIn.jsx
@@ -1,16 +1,97 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 const LogIn = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View style={{marginVertical:100, marginHorizontal:100}}>
-      <Text>LogIn</Text>
-      <Text>LogIn</Text>
-      <Text>LogIn</Text>
-      <Text>LogIn</Text>
-      <Text>LogIn</Text>
-      <Text>LogIn</Text>
+    <View style={styles.container}>
+      <Text style={styles.logo}>Care U Pet</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email..."
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setUsername({ email: text })}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Password..."
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setPassword({ password: text })}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>INGRESAR</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.loginText}>Olvidé mi contraseña</Text>
+      </TouchableOpacity>
+      <View style={{ flexDirection: "row", alignItems: "center", width: '90%' }}>
+        <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
+        <View>
+          <Text style={{ width: 50, textAlign: "center" }}>o</Text>
+        </View>
+        <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
+      </View>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>Continuar con Google</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>Continuar con Facebook</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fb5b5a",
+    marginBottom: 40,
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#f8a",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "white",
+  },
+  loginBtn: {
+    width: "90%",
+    backgroundColor: "#fb5b5a",
+    // borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: "black",
+  },
+});
 
 export default LogIn;
