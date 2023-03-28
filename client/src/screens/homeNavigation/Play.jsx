d5d1ed6c2f753879095180299235d12184f70998
Leandro Pereyra <me.leandrop@gmail.com>
2023-03-27 23:25:38 -0300

    vector-icons PlayScreen progress

--- client/src/screens/homeNavigation/Play.jsx
+++ client/src/screens/homeNavigation/Play.jsx
@@ -1,19 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
-
-const Play = () => {
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from "react-native-vector-icons";
+const Play = ({ coins = 100 }) => {
   return (
     <View style={styles.container}>
       <View style={styles.topbar}>
         <View style={styles.circlesleft}>
-          <Text>Circulitos y $</Text>
+          <View style={styles.circles}></View>
+          <View style={styles.circles}></View>
+          <View style={styles.circles}></View>
+          <View style={styles.circles}></View>
+          <View style={styles.circles}>
+            <MaterialIcons name="attach-money" size={16} />
+          </View>
         </View>
-        <View>
-          <Text>Cerdito 100</Text>
+        <View style={styles.topbarleft}>
+          <View style={styles.pigcircle}>
+            <MaterialCommunityIcons name="piggy-bank-outline" size={24} />
+          </View>
+          <Text style={styles.coins}>{coins}</Text>
         </View>
       </View>
+
       <View style={styles.body}>
         <View style={styles.gamecontainer}></View>
       </View>
+
+      <TouchableOpacity style={styles.footer}>
+        <MaterialIcons name="exit-to-app" size={35} style={styles.exit} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -21,9 +38,98 @@ const Play = () => {
 export default Play;
 
 const styles = StyleSheet.create({
-  container: {},
-  topbar: {},
-  circlesleft: {},
-  body: {},
-  gamecontainer: {},
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    // justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
+  topbar: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    top: 30,
+    backgroundColor: "#EAEAEA",
+    height: "8%",
+    paddingHorizontal: 20,
+  },
+  circlesleft: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  topbarleft: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  circles: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 25,
+    height: 25,
+    borderRadius: 100,
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "black",
+    marginHorizontal: 2,
+  },
+  topbarleft: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 100,
+    height: 35,
+    width: 120,
+  },
+  pigcircle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 100,
+    width: 35,
+    height: 35,
+    marginLeft: -2,
+    backgroundColor: "#D2D2D2",
+  },
+  coins: {
+    marginRight: 10,
+  },
+  body: {
+    backgroundColor: "#D9D9D9",
+    height: "100%",
+    width: "100%",
+    zIndex: -10,
+    display: "flex",
+    // justifyContent: "center",
+    alignItems: "center",
+  },
+  gamecontainer: {
+    height: "70%",
+    width: "80%",
+    backgroundColor: "white",
+    marginTop: 60,
+  },
+  footer: {
+    height: 60,
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    marginTop: -130,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+  },
+  exit: {
+    marginTop: -20,
+    marginRight: 40,
+  },
 });
