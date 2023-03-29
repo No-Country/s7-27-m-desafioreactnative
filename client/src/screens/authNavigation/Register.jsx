e62ebd68f2e4b756500537945a1896d7ef674636
Lalomax <lalomax365@gmail.com>
2023-03-29 17:10:21 -0400

    Fixed and Added modals

--- client/src/screens/authNavigation/Register.jsx
+++ client/src/screens/authNavigation/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PrimaryButton from "../components/PrimaryButton";
 import {
   Text,
   TextInput,
@@ -12,14 +13,16 @@ const Register = () => {
   const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
-      <Text >Registro</Text>
-      
+      <Text style={{ alignSelf: "flex-start", marginLeft: "10%", padding: 10 }}>
+        Registro
+      </Text>
+
       <View style={styles.inputView}>
         <TextInput
           style={styles.inputText}
           placeholder="Email..."
           placeholderTextColor="#003f5c"
-          onChangeText={(text) => setUsername({ email: text })}
+          onChangeText={(text) => setUsername(text)}
         />
       </View>
       <View style={styles.inputView}>
@@ -28,28 +31,39 @@ const Register = () => {
           style={styles.inputText}
           placeholder="Password..."
           placeholderTextColor="#003f5c"
-          onChangeText={(text) => setPassword({ password: text })}
+          onChangeText={(text) => setPassword(text)}
         />
       </View>
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>CREAR CUENTA</Text>
-      </TouchableOpacity>
+      <PrimaryButton
+        text="CREAR CUENTA"
+        handler={() => {
+          // navigation.navigate("Register");
+        }}
+      />
       <TouchableOpacity>
         <Text style={styles.loginText}>Acepto los terminos y condiciones</Text>
       </TouchableOpacity>
-      <View style={{ flexDirection: "row", alignItems: "center", width: '90%' }}>
+      <View
+        style={{ flexDirection: "row", alignItems: "center", width: "80%" }}
+      >
         <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
         <View>
           <Text style={{ width: 50, textAlign: "center" }}>o</Text>
         </View>
         <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
       </View>
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>Continuar con Google</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>Continuar con Facebook</Text>
-      </TouchableOpacity>
+      <PrimaryButton
+        text="Continuar con Google"
+        handler={() => {
+          // navigation.navigate("Register");
+        }}
+      />
+      <PrimaryButton
+        text="Continuar con Facebook"
+        handler={() => {
+          // navigation.navigate("Register");
+        }}
+      />
     </View>
   );
 };
@@ -69,7 +83,7 @@ const styles = StyleSheet.create({
   },
   inputView: {
     width: "80%",
-    backgroundColor: "#f8a",
+    backgroundColor: "#a8a",
     borderRadius: 25,
     height: 50,
     marginBottom: 20,
