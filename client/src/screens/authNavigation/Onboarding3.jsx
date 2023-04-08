10de7ba6c7094bdd1553d35609b98484cc754e16
Ray  Cardenas <115852202+Akeda99@users.noreply.github.com>
2023-04-08 16:47:02 -0500

    Onboardingpost (#36)
    * onboarding dispatch
    
    * authnormal
    OK.

--- client/src/screens/authNavigation/Onboarding3.jsx
+++ client/src/screens/authNavigation/Onboarding3.jsx
@@ -3,28 +3,62 @@ import React, { useState } from 'react'
 import { TextInput } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native';
 import { useRoute } from '@react-navigation/native'
+import { useDispatch } from 'react-redux';
+import { postPetData } from '../../redux/actions/petActions';
 const Onboarding3 = () => {
+  const dispatch = useDispatch();
   const route= useRoute();
+
   const {imagenSeleccionada1,imagenSeleccionada2 }=route.params;
+
     const navigation=useNavigation();
-    
 
     const [nombre, setnombre] = useState('');
+
     const [errorMessage, setErrorMessage] = useState('');
+    
+    let type_mascot= '';
   
+  if(imagenSeleccionada1){
+    type_mascot= 'Gato';
+  } else if(imagenSeleccionada2){
+    type_mascot= 'Perro';
+  }
+
+  //Cuando le da a continuar
     const handlePress = () => {
       if (nombre.trim() === '') {
         setErrorMessage('Por favor ingrese un nombre');
       } else {
         // hacer algo con el valor ingresado
+        dispatch(
+          postPetData({
+            nombre: nombre, // Hasta 25 caracteres
+            tipoMascota: type_mascot, // Id del tipo de mascota
+            caracteristicas: {
+              // Estas o las caracteristicas que se definan
+              edad: 0,
+              sueno: 100,
+              energia: 100,
+              salud: 100,
+              felicidad: 100,
+              higiene: 100
+            },
+            accesoriosEnUso: [] // Array de id's de los accesorios en uso
+          })
+        );
         navigation.navigate('Onboarding4', {nombre,imagenSeleccionada1,imagenSeleccionada2});
       }
     }
+    //Mostrar el nombre en pantalla solamente
     const [nombreMostrado, setNombreMostrado] = useState('');
 
     const mostrarNombre = () => {
       setNombreMostrado(nombre);
     }
+
+ 
+
   return (
     <View style={styles.containerPrincipal}>
       <Text style={{fontWeight: '700', fontSize: 24, textAlign: 'center', marginTop: 20}}>Felicidades! </Text>
