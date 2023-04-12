8ec03b6706787a804ba157ac9c9ef2ea13ae2c0f
AntonelaBonaglia <107816695+AntonelaBonaglia@users.noreply.github.com>
2023-04-12 16:09:57 -0300

    Se edito barra de progreso

--- client/src/screens/components/ProgressCircle.jsx
+++ client/src/screens/components/ProgressCircle.jsx
@@ -19,34 +19,41 @@ const ProgressCircle = ({ progress }) => {
 
   const progressOffset = animatedValue.interpolate({
     inputRange: [0, 100],
-    outputRange: [circumference, 0],
+    outputRange: [352, 0],
   });
 
   const completionStatus = progress === 100 ? "Completed" : "Not completed";
 
   return (
     <View style={styles.container}>
-      <Svg height="80" width="80">
+      <Svg height="100" width="100" style={styles.svg}>
         <Circle
           cx="40"
           cy="40"
           r={radius}
-          stroke="#E0E0E0"
-          strokeWidth="10"
+          stroke="#FFFFFF"
+          strokeWidth="9"
           fill="none"
         />
         <AnimatedCircle
-          cx="40"
+          cx="39"
           cy="40"
           r={radius}
-          stroke="#4245E5"
-          strokeWidth="10"
-          fill="none"
-          strokeDasharray={`${circumference} ${circumference}`}
+          stroke="#798BB2"
+          strokeWidth="7"
+          fill="#798BB2"
+          strokeDasharray={`${circumference} `}
           strokeDashoffset={progressOffset}
           transform={`rotate(-90, 40, 40)`}
         />
-        <Text
+        <View style={styles.bar}>
+          <TouchableOpacity  style={styles.buttonAction} onPress={dormir}>
+            <View>
+              <Image source={isSleeping ? require("../../../assets/dormir.png"): require("../../../assets/despertar.png") } />
+            </View>
+          </TouchableOpacity>
+        </View>
+        {/* <Text
           fill="#000"
           fontSize="15"
           fontWeight="bold"
@@ -55,7 +62,7 @@ const ProgressCircle = ({ progress }) => {
           y="46"
         >
           {`${progress}%`}
-        </Text>
+        </Text> */}
       </Svg>
     </View>
   );
@@ -64,7 +71,14 @@ const ProgressCircle = ({ progress }) => {
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    position:"relative",
+    left: -140,
+      },
+  svg: {
+    position:"relative",
+  
+      },
 });
 
 export default ProgressCircle;
